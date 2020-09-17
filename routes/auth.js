3d081b1dc4b0f153e38039a88a7c6333088d4470
Sam <sam@Sams-MacBook-Air.local>
2020-09-17 04:03:12 +0200

    login/logout

--- routes/auth.js
+++ routes/auth.js
@@ -12,4 +12,10 @@ router.get("/google/callback", passport.authenticate('google', {
     res.redirect('/dashboard')
 });
 
+//logout
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/')
+})
+
 module.exports = router;
