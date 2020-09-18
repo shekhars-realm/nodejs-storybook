3f41bfd7201eabf6dbf8f633fbf33110bd727ed4
Sam <sam@Sams-MacBook-Air.local>
2020-09-18 13:06:57 +0200

    auth check with git

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,9 @@ router.get('/dashboard', ensureAuth, (req, res) => {
     res.render('dashboard')
 })
 
+
+router.get("/profile", ensureAuth, (req, res) => {
+  res.render("dashboard");
+});
+
 module.exports = router
