389c7080e1fb4a90f5e540d51c9e0ad65a905b97
Sam <sam@Sams-MacBook-Air.local>
2020-09-20 03:33:43 +0200

    routes for stories

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const {ensureAuth, ensureGuest} = require('../middleware/auth')
 
+const Story = require('../models/Story')
+
 //login/landing pagerouter.get()
 
 router.get('/', ensureGuest, (req, res) => {
@@ -11,19 +13,22 @@ router.get('/', ensureGuest, (req, res) => {
 })
 
 
-router.get('/dashboard', ensureAuth, (req, res) => {
-    res.render('dashboard')
+router.get('/dashboard', ensureAuth, async (req, res) => {
+
+    try {
+      const stories = await Story.find({user: req.user.id}).lean()
+      res.render('dashboard', {
+        name: req.user.firstName,
+        stories
+      })
+    } catch (error) {
+      console.error(error)
+      res.render('/error/500')
+    }
+
 })
 
 
-router.get("/profile", ensureAuth, (req, res) => {
-  res.render("dashboard");
-});
-
-
-router.get("/profile", ensureAuth, (req, res) => {
-  res.render("dashboard");
-});
 
 
 module.exports = router
---
+++ routes/stories.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuth } = require('../middleware/auth')
+
+const Story = require('../models/Story')
+
+//Show add page
+
+router.get('/add', ensureAuth, (req, res) => {
+    res.render('stories/add')
+})
+
+module.exports = router
